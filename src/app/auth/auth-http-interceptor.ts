1103d261d0424c8e6db65e35aa953a92c203d80a
lethalSid <senien.hicks@gmail.com>
2020-08-24 10:58:22 -0400

    committing authentication system

---
+++ src/app/auth/auth-http-interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpEventType,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap, filter } from 'rxjs/operators';
+
+@Injectable()
+export class AuthHttpInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // Modify or log the outgoing request
+    const modifiedReq = req.clone({
+      withCredentials: true,
+    });
+
+    return next.handle(modifiedReq)
+      // .pipe(
+      //   filter(value => value.type === HttpEventType.Sent),
+      //   tap(value => {
+      //     console.log(('Sent the request'));
+      //   })
+      // );
+  }
+}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { MatchPassword } from "../validators/match-password";
-import { UniqueUsername } from '../validators/unique-username';
+import { UniqueUsername } from "../validators/unique-username";
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: "app-signup",
@@ -11,13 +12,16 @@ import { UniqueUsername } from '../validators/unique-username';
 export class SignupComponent implements OnInit {
   authForm = new FormGroup(
     {
-      username: new FormControl("", [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(20),
-        Validators.pattern(/^[a-z0-9]+$/),
-      ],
-        [this.uniqueUsername.validate]),
+      username: new FormControl(
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/),
+        ],
+        [this.uniqueUsername.validate]
+      ),
       password: new FormControl("", [
         Validators.required,
         Validators.minLength(4),
@@ -33,8 +37,30 @@ export class SignupComponent implements OnInit {
   );
   constructor(
     private matchPassword: MatchPassword,
-    private uniqueUsername: UniqueUsername
-  ) { }
+    private uniqueUsername: UniqueUsername,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {}
+
+  onSubmit() {
+    if (this.authForm.invalid) {
+      return;
+    }
+
+    this.authService.signup(this.authForm.value)
+      .subscribe({
+        next: response => {
+         //Navigate to some other route
+
+        },
+        error: (error) => {
+          if (!error.status) {
+            this.authForm.setErrors({ noConnection: true })
+          } else {
+            this.authForm.setErrors({ unknownError: true})
+          }
+        }
+      });
+  }
 }
